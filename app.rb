04006711c6d09591242bde2751debe8bf17c3027
ruthmesfun <ruth.mesfun@gmail.com>
2018-11-19 11:09:23 -0500

    Done

--- app.rb
+++ app.rb
@@ -2,5 +2,42 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  
+  get '/reversename/:name' do 
+    @reversed_name = params[:name].reverse
+    "#{@reversed_name }"
+  end
 
+  get '/square/:number' do 
+    @squared = params[:number].to_i * params[:number].to_i
+
+    "#{@squared}"
+  end
+
+  get '/say/:number/:phrase' do 
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+   
+    "#{@phrase * @number}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    @sentence = "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+
+    "#{@sentence}"
+  end
+
+  get '/:operation/:number1/:number2' do 
+    if params[:operation] == "add"
+      @answer = params[:number1].to_i + params[:number2].to_i
+    elsif params[:operation] == "subtract"
+      @answer = params[:number1].to_i - params[:number2].to_i
+    elsif params[:operation] == "multiply"
+      @answer = params[:number1].to_i * params[:number2].to_i
+    elsif params[:operation] == "divide"
+      @answer = params[:number1].to_i / params[:number2].to_i
+    end
+
+    "#{@answer}"
+  end
 end
